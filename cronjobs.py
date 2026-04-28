a40eac466bfaf8e0f300061658e3433375194b29
Krystian Wierciak <77780238+KryhaX@users.noreply.github.com>
2026-04-28 14:26:51 +0200

    Log execution time for hourly task
    Added print statement to log hourly task execution time. Updatedd to latest version

--- cronjobs.py
+++ cronjobs.py
@@ -1,14 +1,34 @@
 from __future__ import print_function
-
 import schedule
 import time
 import os
+import sys
 
 def hourly():
-    os.system('powershift image jobs hourly')
+
+    print("Task cron: Hourly task executed at", time.strftime("%Y-%m-%d %H:%M:%S"))
 
 schedule.every().hour.do(hourly)
+
+print("Cron scheduler started...")
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+
+# Old Code
+# from __future__ import print_function
+
+# import schedule
+# import time
+# import os
+
+# def hourly():
+#     os.system('powershift image jobs hourly')
+
+# schedule.every().hour.do(hourly)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
